7b2d6cbfe43ee8f6d38f83c4d52c1f84ad284d63
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2022-09-26 15:24:25 -0300

    feat: first dag

--- airflow/dags/teste.py
+++ airflow/dags/teste.py
@@ -42,7 +42,6 @@ BASE_DIR = tempfile.gettempdir()
 
 with DAG(
     dag_id='example_python_operator',
-    schedule=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
     tags=['example'],
